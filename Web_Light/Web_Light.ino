fb75570a8a63764ba7cb88d0f57b893fc946f16b
forsash <alfor-4@yandex.ru>
2016-12-28 17:13:56 +0300

    working config

--- Web_Light/Web_Light.ino
+++ Web_Light/Web_Light.ino
@@ -25,7 +25,7 @@ WebServer webserver(PREFIX, 80);
 
 unsigned long int val = 0;            //integer for brightness level
 unsigned long int prevval=val;
-unsigned int l[6]= {0,1,127,191,255,600}; //val,l1,l2,l3,l4,off_delay
+unsigned int l[6]= {0,0,0,0,255,600}; //val,l1,l2,l3,l4,off_delay
 
 unsigned int off_delay = 600; //interval for on in seconds
 unsigned long int prev=0,cur=0; //timestamps
@@ -51,31 +51,31 @@ void ctrlCmd(WebServer &server, WebServer::ConnectionType type, char *, bool)
           {
           int var=strtoul(value, NULL, 10);
           l[1] = constrain(255*var/100,MIN,255);
-          EEPROM.update(L1ADDR, l[1]);
+        //  EEPROM.update(L1ADDR, l[1]);
           }
        if (strcmp(p_name, "l2") == 0)
           {
           int var=strtoul(value, NULL, 10);
           l[2] = constrain(255*var/100,l[1],255);
-          EEPROM.update(L2ADDR, l[2]);
+      //    EEPROM.update(L2ADDR, l[2]);
           }
        if (strcmp(p_name, "l3") == 0)
           {
           int var=strtoul(value, NULL, 10);
           l[3] = constrain(255*var/100,l[2],255);
-          EEPROM.update(L3ADDR, l[3]);
+       //   EEPROM.update(L3ADDR, l[3]);
           }
         if (strcmp(p_name, "l4") == 0)
           {
           int var=strtoul(value, NULL, 10);
           l[4] = constrain(255*var/100,l[3],255);
-          EEPROM.update(L4ADDR, l[4]);
+       //   EEPROM.update(L4ADDR, l[4]);
           }
         if (strcmp(p_name, "del") == 0)
           {
           off_delay = strtoul(value, NULL, 10);
           l[5]=off_delay;
-          EEPROM.update(DELADDR, off_delay);
+      //    EEPROM.update(DELADDR, off_delay);
           }
         }
     } while (repeat);
@@ -138,7 +138,7 @@ void ctrlCmd(WebServer &server, WebServer::ConnectionType type, char *, bool)
   "<button type=\"button\" class=\"cfg\" style='font-size:200%;height:100px;width:100px\'>cfg</button>"
   "</td></tr></table>";
   server.printP(message);
-  l[0]=val;
+  //l[0]=val;
   server.print("<table><tr><td align=center width=50%>current level=");
   server.print((100*l[0])/255);
   server.print("</td><td width=50% align=center> timeout=");
@@ -192,19 +192,27 @@ void cfgCmd(WebServer &server, WebServer::ConnectionType type, char *, bool)
       do{Serial.print(value[i]);i++;}while(value[i]);
       Serial.println();
       
-      //if(vi>0)
-       // {    
+      if(vi>0&&vi<5)
+       {    
         var=strtol(value, NULL, 10);
         Serial.print("var=");
         Serial.print(var);
-        l[vi+1] = constrain(var,0,100);
+        Serial.print("%");
+        
+        if((vi+1)<5){l[vi+1] = constrain((255*var)/100,0,255);}
+        if((vi+1)==5){l[vi+1] = constrain(var,0,255);}
+        
         Serial.print(" Writing eeprom l");
         Serial.print(vi+1);
         Serial.print("=");
         Serial.print(l[vi+1]);
+        Serial.print("(");
+        if((vi+1)<5){Serial.print((100*l[vi+1])/255);}
+        if((vi+1)==5){Serial.print(l[vi+1]);}
+        Serial.print("%)");
         Serial.println();
         EEPROM.update(vi,l[vi+1]);
-      //  }
+       }
       vi++;
       if (strcmp(p_name, "val") == 0)
         {
@@ -241,9 +249,9 @@ void setup()
 
   //get settings from EEPROM
   l[1]=constrain(EEPROM.read(L1ADDR),MIN,255);
-  l[2]=constrain(EEPROM.read(L2ADDR),l[2],255);
-  l[3]=constrain(EEPROM.read(L3ADDR),l[3],255);
-  l[4]=constrain(EEPROM.read(L4ADDR),l[4],255);
+  l[2]=constrain(EEPROM.read(L2ADDR),0,255);
+  l[3]=constrain(EEPROM.read(L3ADDR),0,255);
+  l[4]=constrain(EEPROM.read(L4ADDR),0,255);
   off_delay=EEPROM.read(DELADDR);
   l[5]=off_delay;
   //Open serial
